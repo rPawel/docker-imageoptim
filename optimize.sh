fe1f3eb18dde629f5decb38af423c2d192c847cb
root <pawel@profimedia.co.uk>
2018-04-15 19:23:04 +0100

    fix for spaces in filenames

--- optimize.sh
+++ optimize.sh
@@ -3,19 +3,19 @@ set -euo pipefail
 
 function optimizeFile {
 
-inFile=$1
+inFile="$1"
 infileExt=${inFile##*\.}
 shopt -s nocasematch
 
 case "$infileExt" in
         png) /usr/bin/optipng -preserve -quiet "$inFile"
            ;;
-        jpg) /usr/bin/jpegoptim --preserve-perms --preserve --strip-all --quiet "$inFile"
+        jpg) /usr/bin/jpegoptim --preserve-perms --preserve --strip-com --strip-exif --strip-iptc --strip-xmp --quiet "$inFile"
            ;;
         *) echo " $inFile : This should never happen" && exit 1
            ;;
 esac
 }
 
-optimizeFile $1
+optimizeFile "$1"
 
